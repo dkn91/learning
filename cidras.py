40236af5a2fdfc27ef5c88a1c5666daba19fe84a
Ubuntu <dpishe@pycent.c1m4jlrjjb0uzdh30faehs41ia.dx.internal.cloudapp.net>
2016-05-21 23:11:10 +0000

    updated cidras.py

--- cidras.py
+++ cidras.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+
 
 ''' This module accepts AS Number from user and returns
 the Company name that bought the AS and total population of that city.'''
 import string
 import urllib
+import sys
+import argparse
+import pprint
 
-   
 def fetch_zipage(as_no):
     '''This function fetches the entire webpage for the as number.
     It filters out data from the html coded page line by line.
@@ -22,3 +26,16 @@ def fetch_zipage(as_no):
             #print temp[a+4:b], 'inras'
             return temp[a+4:b]
             break
+
+def main():
+	parser = argparse.ArgumentParser(description='Process asn')
+	parser.add_argument('-asn', type=str, help='expecting a valid asn')
+	args = parser.parse_args()
+	data = {}
+	asnlist = args.asn.split(',')
+	for i in asnlist:
+		data[i] = fetch_zipage(i)
+		
+	pprint.pprint(data)
+
+main()
